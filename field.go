a977deca425f5339f5d5ee576a5000ea06b8bcde
phil lisitza <phil.lisitza@twopoint.com>
2022-04-09 08:43:39 -0500

    chore: updating module info

--- field.go
+++ field.go
@@ -2,7 +2,7 @@ package golevel7
 
 import (
 	"fmt"
-	"golevel7/commons"
+	"github.com/twopt/golevel7/commons"
 	"strings"
 )
 
--- messageScanner.go
+++ messageScanner.go
@@ -2,7 +2,7 @@ package golevel7
 
 import (
 	"bufio"
-	"golevel7/commons"
+	"github.com/twopt/golevel7/commons"
 	"io"
 )
 
